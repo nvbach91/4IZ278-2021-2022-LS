649272b87951c05739370a1a68950e5ea3996690
0nmus <91312612+0nmus@users.noreply.github.com>
2022-03-10 10:53:43 +0100

    muso00-cv03
    Odevzdání úkolu č.3
    https://eso.vse.cz/~muso00/cv03/

---
+++ www/muso00/cv03/registration-success.php
@@ -0,0 +1,6 @@
+<?php include './includes/head.php'; ?>
+<header>
+    <p class="reg-success"> You have successfully registered for the card tournament!</p>
+</header>
+<?php require './index.php'; ?>
+<?php include './includes/foot.php'; ?>
