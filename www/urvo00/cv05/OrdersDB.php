e3ad6191a6b08b12c03d672d87ad6de9f0a8862f
Ashley-Unicorn <91244741+Ashley-Unicorn@users.noreply.github.com>
2022-03-23 19:40:00 +0100

    urvo00-cv05

---
+++ www/urvo00/cv05/OrdersDB.php
@@ -0,0 +1,27 @@
+<?php require './Database.php'; ?>
+<?php
+class OrdersDB extends Database{
+    protected $tableName = 'orders';
+    public function fetchAll(){
+        $statement = $this -> pdo -> prepare("SELECT * FROM " . $this-> tableName .";");
+        $statement -> execute();
+        return $statement -> fetchAll();
+    }
+    public function fetchById($id){
+        $statement = $this -> pdo -> prepare("SELECT * FROM" . $this-> tableName . "WHERE user_id = " . $id .";");
+        $statement -> execute();
+    }
+    public function updateById($id, $field, $newValue){
+        $statement = $this -> pdo -> prepare("UPDATE" . $this-> tableName . "SET" . $field . "= '" . $newValue . "' WHERE user_id = " . $id .";");
+        $statement -> execute();
+    }
+    public function create($args){
+        $statement = $this -> pdo -> prepare("INSERT INTO " . $this-> tableName ." (number, date) ". " VALUES ('".$args['number']."','". $args['date'] ."');");
+        $statement -> execute();
+    }
+    public function deleteById($id){
+        $statement = $this -> pdo -> prepare("DELETE FROM" . $this-> tableName . "WHERE user_id = " . $id .";");
+        $statement -> execute();
+    }
+}
+?>
---
+++ www/urvo00/cv05/products.php
@@ -0,0 +1,23 @@
+<?php require './ProductsDB.php' ?>
+<?php
+    $productsDB = new ProductsDB();
+    $products = $productsDB -> fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($products as $product):?>
+        <div>
+            <?php echo $product['name'];?>
+            <br>
+            <?php echo $product['price'];?>
+    </div>
+    <?php endforeach ; ?>
+</body>
+</html>
