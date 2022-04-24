a957142e2b7f7d76f5e292860cf903e594480548
Marek Mikula <marek.mikula01@gmail.com>
2022-04-24 20:37:43 +0200

    feat: default branches seeder, app install command

--- www/mikm25/sp/database/seeders/BranchesSeeder.php
+++ www/mikm25/sp/database/seeders/BranchesSeeder.php
@@ -2,12 +2,59 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
 use Illuminate\Database\Seeder;
 
 class BranchesSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $branches = [
+            'administration',
+            'tourism',
+            'economy',
+            'gastronomy',
+            'it',
+            'marketing',
+            'security',
+            'food_industry',
+            'services',
+            'state_services',
+            'top_management',
+            'production_industry',
+            'agriculture',
+            'auto_moto',
+            'chemistry',
+            'electronics',
+            'it_consultation',
+            'culture',
+            'media_hr_advertising',
+            'hr',
+            'sales',
+            'construction',
+            'development',
+            'publishing',
+            'science',
+            'customer_service',
+            'banking',
+            'logistics',
+            'pharmacy',
+            'it_management',
+            'quality_control',
+            'purchase',
+            'legal',
+            'insurance',
+            'engineering',
+            'telecommunication',
+            'education',
+            'healthcare',
+            'craft',
+        ];
+
+        foreach ($branches as $branch) {
+            $model = new Branch();
+            $model->name = $branch;
+            $model->save();
+        }
     }
 }
