6febeeedad6ea4686ebb76a07ebe0003d0f9bd34
Marek Mikula <marek.mikula01@gmail.com>
2022-05-01 20:31:31 +0200

    feat: user id query

--- www/mikm25/sp/app/Models/Builders/PositionBuilder.php
+++ www/mikm25/sp/app/Models/Builders/PositionBuilder.php
@@ -4,15 +4,12 @@ namespace App\Models\Builders;
 
 use App\Models\Builders\Traits\WithIdQuery;
 use App\Models\Builders\Traits\WithDateQueries;
+use App\Models\Builders\Traits\WithUserIdQuery;
 use Illuminate\Database\Eloquent\Builder;
 
 class PositionBuilder extends Builder
 {
     use WithIdQuery;
     use WithDateQueries;
-
-    public function ofUserId(int $userId): self
-    {
-        return $this->where('user_id', $userId);
-    }
+    use WithUserIdQuery;
 }
