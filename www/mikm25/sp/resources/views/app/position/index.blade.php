fd7d07aaef79a90d3db2ddccd7fac2a2e80862fd
Marek Mikula <marek.mikula01@gmail.com>
2022-05-01 20:04:28 +0200

    feat: removed background of position index

--- www/mikm25/sp/resources/views/app/position/index.blade.php
+++ www/mikm25/sp/resources/views/app/position/index.blade.php
@@ -9,63 +9,61 @@
 @endsection
 
 @section('app-content')
-    <div class="p-2 bg-light rounded-2">
-        <div class="row">
-            <div class="col">
-                <table class="table table-borderless table-responsive">
-                    <thead>
+    <div class="row">
+        <div class="col">
+            <table class="table table-borderless table-responsive">
+                <thead>
+                <tr>
+                    <th>{{ __('models.id') }}</th>
+                    <th>{{ __('models.position.name') }}</th>
+                    <th>{{ __('models.position.branch') }}</th>
+                    <th>{{ __('models.position.salary_from') }}</th>
+                    <th>{{ __('models.position.salary_to') }}</th>
+                    <th>{{ __('models.position.external_url') }}</th>
+                    <th>{{ __('models.position.valid') }}</th>
+                    <th>{{ __('models.position.company_name') }}</th>
+                    <th>{{ __('models.position.clicks') }}</th>
+                    <th>{{ __('models.position.reactions') }}</th>
+                    <th>{{ __('tables.actions') }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($positions as $position)
                     <tr>
-                        <th>{{ __('models.id') }}</th>
-                        <th>{{ __('models.position.name') }}</th>
-                        <th>{{ __('models.position.branch') }}</th>
-                        <th>{{ __('models.position.salary_from') }}</th>
-                        <th>{{ __('models.position.salary_to') }}</th>
-                        <th>{{ __('models.position.external_url') }}</th>
-                        <th>{{ __('models.position.valid') }}</th>
-                        <th>{{ __('models.position.company_name') }}</th>
-                        <th>{{ __('models.position.clicks') }}</th>
-                        <th>{{ __('models.position.reactions') }}</th>
-                        <th>{{ __('tables.actions') }}</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($positions as $position)
-                        <tr>
-                            <td>{{ $position->id }}</td>
-                            <td>{{ $position->name }}</td>
-                            <td>{{ $position->branch->translated_name }}</td>
-                            <td>{{ $position->salary_from }}</td>
-                            <td>{{ $position->salary_to }}</td>
-                            <td>
-                                @if(!empty($position->external_url))
-                                    <a href="{{ $position->external_url }}" target="_blank"
-                                       title="{{ $position->external_url }}">
-                                        {{ __('common.link') }}
-                                    </a>
-                                @endif
-                            </td>
-                            <td>
-                                @if($position->is_valid)
-                                    <span class="text-success">{{ __('common.yes') }}</span>
-                                @else
-                                    <span class="text-danger">{{ __('common.no') }}</span>
-                                @endif
-                            </td>
-                            <td>{{ $position->company_name }}</td>
-                            <td>{{ $position->clicks_count }}</td>
-                            <td>{{ $position->reactions_count }}</td>
-                            <td>
-                                <a href="{{ route('app.positions.detail', ['position' => $position->id, 'tab' => \App\Constants\PositionTabConstants::TAB_DETAIL]) }}"
-                                   class="btn btn-sm btn-primary">
-                                    {{ __('common.buttons.detail') }}
+                        <td>{{ $position->id }}</td>
+                        <td>{{ $position->name }}</td>
+                        <td>{{ $position->branch->translated_name }}</td>
+                        <td>{{ $position->salary_from }}</td>
+                        <td>{{ $position->salary_to }}</td>
+                        <td>
+                            @if(!empty($position->external_url))
+                                <a href="{{ $position->external_url }}" target="_blank"
+                                   title="{{ $position->external_url }}">
+                                    {{ __('common.link') }}
                                 </a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
+                            @endif
+                        </td>
+                        <td>
+                            @if($position->is_valid)
+                                <span class="text-success">{{ __('common.yes') }}</span>
+                            @else
+                                <span class="text-danger">{{ __('common.no') }}</span>
+                            @endif
+                        </td>
+                        <td>{{ $position->company_name }}</td>
+                        <td>{{ $position->clicks_count }}</td>
+                        <td>{{ $position->reactions_count }}</td>
+                        <td>
+                            <a href="{{ route('app.positions.detail', ['position' => $position->id, 'tab' => \App\Constants\PositionTabConstants::TAB_DETAIL]) }}"
+                               class="btn btn-sm btn-primary">
+                                {{ __('common.buttons.detail') }}
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
-        {{ $positions->links() }}
     </div>
+    {{ $positions->links() }}
 @endsection
