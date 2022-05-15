a8d7977fd67af856498d443e8eff7c560b16ac45
Matěj Zavadil <speedajax13@gmail.com>
2022-05-15 16:53:18 +0200

    zavm07-sp
    Pilotní verze

---
+++ www/zavm07/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <title>Driedmeats</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./boostrap/css/bootstrap.css" rel="stylesheet">
+    <script src="./boostrap/js/bootstrap.bundle.js"></script>
+</head>
+<body style="color: white">
+    <nav class="navbar navbar-dark navbar-expand-sm sticky-top h4" style="background: #AD2E24;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Logo
+            </a>
+            <ul class="navbar-nav d-flex " style="">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Kategorie</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Hovězí</a></li>
+                        <li><a class="dropdown-item" href="#">Vepřové</a></li>
+                        <li><a class="dropdown-item" href="#">Krůtí</a></li>
+                        <li><a class="dropdown-item" href="#">Zvěřinové</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Přihlásit se</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registrace</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Košík</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="d-flex w-75 m-auto min-vh-100 justify-content-center flex-wrap">
+            <div class="card m-5" style="width:20%;height: 400px">
+                <img class="card-img-top" src="./res/test.jpeg" alt="Card image">
+                <div class="card-body" style="color: black">
+                    <h4 class="card-title">Jerky1</h4>
+                    <p class="card-text">Jsou fakt dobrý</p>
+                    <h5>40 g</h5>
+                    <h4>50 Kč</h4>
+                    <a href="#" class="btn btn-primary" style="background: #AD2E24;">Přidat do košíku</a>
+                </div>
+            </div>
+
+        <div class="card m-5" style="width:20%;height: 400px">
+            <img class="card-img-top" src="./res/test.jpeg" alt="Card image">
+            <div class="card-body" style="color: black">
+                <h4 class="card-title">Jerky1</h4>
+                <p class="card-text">Jsou fakt dobrý</p>
+                <h5>40 g</h5>
+                <h4>50 Kč</h4>
+                <a href="#" class="btn btn-primary" style="background: #AD2E24;">Přidat do košíku</a>
+            </div>
+        </div>
+        <div class="card m-5" style="width:20%;height: 400px">
+            <img class="card-img-top" src="./res/test.jpeg" alt="Card image">
+            <div class="card-body" style="color: black">
+                <h4 class="card-title">Jerky1</h4>
+                <p class="card-text">Jsou fakt dobrý</p>
+                <h5>40 g</h5>
+                <h4>50 Kč</h4>
+                <a href="#" class="btn btn-primary" style="background: #AD2E24;">Přidat do košíku</a>
+            </div>
+        </div>
+        <div class="card m-5" style="width:20%;height: 400px">
+            <img class="card-img-top" src="./res/test.jpeg" alt="Card image">
+            <div class="card-body" style="color: black">
+                <h4 class="card-title">Jerky1</h4>
+                <p class="card-text">Jsou fakt dobrý</p>
+                <h5>40 g</h5>
+                <h4>50 Kč</h4>
+                <a href="#" class="btn btn-primary" style="background: #AD2E24;">Přidat do košíku</a>
+            </div>
+        </div>
+        <div class="card m-5" style="width:20%;height: 400px">
+            <img class="card-img-top" src="./res/test.jpeg" alt="Card image">
+            <div class="card-body" style="color: black">
+                <h4 class="card-title">Jerky1</h4>
+                <p class="card-text">Jsou fakt dobrý</p>
+                <h5>40 g</h5>
+                <h4>50 Kč</h4>
+                <a href="#" class="btn btn-primary" style="background: #AD2E24;">Přidat do košíku</a>
+            </div>
+        </div>
+        <div class="card m-5" style="width:20%;height: 400px">
+            <img class="card-img-top" src="./res/test.jpeg" alt="Card image">
+            <div class="card-body" style="color: black">
+                <h4 class="card-title">Jerky1</h4>
+                <p class="card-text">Jsou fakt dobrý</p>
+                <h5>40 g</h5>
+                <h4>50 Kč</h4>
+                <a href="#" class="btn btn-primary" style="background: #AD2E24;">Přidat do košíku</a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <footer>
+        <div class="container-fluid text-center p-5" style="background: #AD2E24;"> _placeholder_</div>
+        <div class="container-fluid text-center p-3" style="background: #540804;"> &copy; 2022 Matěj Zavadil</div>
+    </footer>
+</body>
+</html>
