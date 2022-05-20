1eba45fff76707b90078335ef9802f35b0b11a8f
Marek Mikula <marek.mikula01@gmail.com>
2022-05-21 00:56:49 +0200

    feat: button moved to footer

--- www/mikm25/sp/resources/views/auth/login.blade.php
+++ www/mikm25/sp/resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
                     <a href="{{ route('auth.forgotten-password.form') }}">{{ __('login.forgotten_password') }}</a>
                 </div>
 
-                <div class="d-flex justify-content-between mb-3">
+                <div class="d-flex justify-content-between">
                     <div>
                         <a href="{{ route('landing-page') }}" class="btn btn-light">
                             <i class="bi bi-house"></i>
@@ -63,13 +63,14 @@
                         {{ __('login.submit') }}
                     </button>
                 </div>
-
-                <div class="d-grid">
-                    <a href="{{ route('auth.facebook.redirect') }}" class="btn btn-primary d-blo">
-                        <i class="bi bi-facebook"></i> {{ __('login.facebook') }}
-                    </a>
-                </div>
             </form>
+        </div>
+        <div class="card-footer">
+            <div class="d-grid">
+                <a href="{{ route('auth.facebook.redirect') }}" class="btn btn-primary d-blo">
+                    <i class="bi bi-facebook"></i> {{ __('login.facebook') }}
+                </a>
+            </div>
         </div>
     </div>
 @endsection
